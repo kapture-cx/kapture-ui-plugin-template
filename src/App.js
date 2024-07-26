aa3b7319166f159e8519db2a62584c1389dd319c
Joel DSouza <joel.dsouza@kapturecrm.com>
2024-07-26 16:59:14 +0530

    remove theme provider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Suspense } from "react"
-import { ThemeProvider } from "@material-ui/styles"
 import Container from "./container/container"
 
 function App(props) {
     return (
-        <ThemeProvider>
-            <Suspense fallback={<div>Loading...</div>}>
-                <div id="">
-                    <Container {...props} />
-                </div>
-            </Suspense>
-        </ThemeProvider>
+        <Suspense fallback={<div>Loading...</div>}>
+            <div id="">
+                <Container {...props} />
+            </div>
+        </Suspense>
     )
 }
 
