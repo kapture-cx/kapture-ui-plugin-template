9257ae9744df595bcf37dc4a84cff0d1a8a62a36
Joel DSouza <joel.dsouza@kapturecrm.com>
2024-07-26 18:05:20 +0530

    added stylesprovider and plugin specific prefix

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, { Suspense } from "react"
 import { ThemeProvider } from "@material-ui/styles"
+import { StylesProvider, jssPreset, createGenerateClassName } from "@material-ui/core/styles"
+import { create } from "jss"
+import jssExtend from "jss-extend"
 import Container from "./container/container"
+
+const jss = create({
+    ...jssPreset(),
+    plugins: [...jssPreset().plugins, jssExtend()],
+    insertionPoint: document.getElementById("jss-insertion-point"),
+})
+const generateClassName = createGenerateClassName({ productionPrefix: "kapture-plugin" })
 
 function App(props) {
     return (
-        <ThemeProvider>
-            <Suspense fallback={<div>Loading...</div>}>
-                <div id="">
-                    <Container {...props} />
-                </div>
-            </Suspense>
-        </ThemeProvider>
+        <StylesProvider jss={jss} generateClassName={generateClassName}>
+            <ThemeProvider>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <div id="">
+                        <Container {...props} />
+                    </div>
+                </Suspense>
+            </ThemeProvider>
+        </StylesProvider>
     )
 }
 
