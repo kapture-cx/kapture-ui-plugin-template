0647334eb5729410fe27199fae8cfc7552d9fcd2
Joel DSouza <joel.dsouza@kapturecrm.com>
2024-07-26 17:42:55 +0530

    re-add themeprovider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Suspense } from "react"
+import { ThemeProvider } from "@material-ui/styles"
 import Container from "./container/container"
 
 function App(props) {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <div id="">
-                <Container {...props} />
-            </div>
-        </Suspense>
+        <ThemeProvider>
+            <Suspense fallback={<div>Loading...</div>}>
+                <div id="">
+                    <Container {...props} />
+                </div>
+            </Suspense>
+        </ThemeProvider>
     )
 }
 
